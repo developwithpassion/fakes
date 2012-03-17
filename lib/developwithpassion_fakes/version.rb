3a67e5d1458e679579987cb3a072a5e2fab13a58
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-03-16 22:52:19 -0600

    Added the ability for the ignore set to capture multiple sets of arguments

--- lib/developwithpassion_fakes/version.rb
+++ lib/developwithpassion_fakes/version.rb
@@ -1,5 +1,5 @@
 module DevelopWithPassion
   module Fakes
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
   end
 end
--- spec/specs/ignore_set_spec.rb
+++ spec/specs/ignore_set_spec.rb
@@ -17,6 +17,26 @@ module DevelopWithPassion
           sut.matches?(3,"hello",4,5).should be_true
         end
       end
+
+      context "when capturing a set of arguments" do
+        before (:each) do
+          sut.capture_args(1)
+          sut.capture_args(2)
+        end
+        it "should store a list for each set of arguments" do
+          sut.arg_sets.count.should == 2
+        end
+      end
+
+      context "when determining if it was called with a set of arguments" do
+        before (:each) do
+          sut.capture_args(1)
+          sut.capture_args(2)
+        end
+        it "should match if any of its argument sets match" do
+          sut.was_called_with?(2).should be_true
+        end
+      end
     end
 
   end
